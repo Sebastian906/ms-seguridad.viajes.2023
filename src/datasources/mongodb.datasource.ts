05dd12ef0d65b4dfa94892b4dc06c967c4083718
Gsus <js49831@gmail.com>
2023-09-21 08:51:09 -0500

    creacion de datasource, coneccion a base de datos  mongodb

---
+++ src/datasources/mongodb.datasource.ts
@@ -0,0 +1,34 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'mongodb',
+  connector: 'mongodb',
+  url: 'mongodb+srv://<username>:<password>@cluster0.jzqbrwr.mongodb.net/seguridad_viajes?retryWrites=true&w=majority',
+  host: 'localhost',
+  port: 27017,
+  user: '',
+  password: '',
+  database: 'seguridad_viajes',
+  useNewUrlParser: true,
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class MongodbDataSource
+  extends juggler.DataSource
+  implements LifeCycleObserver
+{
+  static dataSourceName = 'mongodb';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.mongodb', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
