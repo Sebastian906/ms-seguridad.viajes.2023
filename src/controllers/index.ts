2e16b1b1901793996c1ff6df5fdeeccbb8689461
Cristian Garcia <cristiangarcianastar21@gmail.com>
2023-09-22 16:37:40 -0500

    Creacion de controladores de seguridad

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -5,3 +5,7 @@ export * from './role-permissions.controller';
 export * from './permissions-role.controller';
 export * from './role-user.controller';
 export * from './user-role.controller';
+export * from './user.controller';
+export * from './permissions.controller';
+export * from './login.controller';
+export * from './role.controller';
